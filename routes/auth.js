71bd09abe6a7e49dbb0107022987d0730973a0eb
David <41311088+biagiola@users.noreply.github.com>
2020-05-07 11:35:51 -0400

    encrypted password added
    npm install bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const { registerValidation }= require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -11,10 +12,14 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({ email: req.body.email });
     if(emailExist) return res.status(400).send('Email already exists');
 
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try{
