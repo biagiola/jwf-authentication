1daa910eaf6a107131379744687f04896368c8e8
David <41311088+biagiola@users.noreply.github.com>
2020-06-01 14:02:13 -0400

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,20 @@
 const router = require('express').Router();
 const User = require('../model/User');
-
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required()
-}); 
+const { registerValidation, loginValidation } = require('../validation');
  
 router.post('/register', async (req, res) => {
 
     // lest validate the date beore we a user
     //const validation = Joi.validate(req.body, schema);
-    const { error } = schema.validate(req.body);
+    const { error } = registerValidation(req.body);
 
-    if(error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    //Cheking if the user is already in the databa
+    const emailExist = await User.findOne({ email: req.body.email });
+    if(emailExist) return res.status(400).send('Email already exists');
 
+    //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
