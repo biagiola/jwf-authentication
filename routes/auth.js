3d4c6a00a48760c7d0e1f8873b14712a0d31149c
David <41311088+biagiola@users.noreply.github.com>
2020-06-01 14:56:06 -0400

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,54 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const { registerValidation, loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
  
 router.post('/register', async (req, res) => {
 
     // lest validate the date beore we a user
     //const validation = Joi.validate(req.body, schema);
     const { error } = registerValidation(req.body);
-
     if (error) return res.status(400).send(error.details[0].message);
 
-    //Cheking if the user is already in the databa
+    //Cheking if the user is already in the data base
     const emailExist = await User.findOne({ email: req.body.email });
     if(emailExist) return res.status(400).send('Email already exists');
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
     //Create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
 
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user._id });
     } catch(err) {
         res.status(400).send(err)
     }
+});
+
+router.post('/login', async (req, res) => {
+
+    // lest validate the data before we a user
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Cheking if the user is already in the data base
+    const user = await User.findOne({ email: req.body.email });
+    if(!user) return res.status(400).send('Email or password is wrong');
+
+    //Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Email is not found');
+
+    res.send('Logged in!');
+    
 });
 
 module.exports = router;
